4a1afe75efe3ae13215e059ad51cbf494b16cdac
이창준 <nomatterjun@gmail.com>
2023-06-01 00:36:09 +0900

    Fix Package setup

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Composer",
-    platforms: [
-      .iOS(.v15)
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
